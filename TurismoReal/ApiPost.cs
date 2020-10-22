30d0d333acd5321202cb45116f0e1dadec1e32c8
anais <ana.acevedo@alumnos.duoc.cl>
2020-10-22 01:29:05 -0300

    creo CRD de la api

---
+++ TurismoReal/ApiPost.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TurismoReal
+{
+    //---BTN AGREGAR---
+    public class ApiPost
+    {
+        public dynamic Agregar(string url, string json, string autorizacion = null)
+        {
+            try
+            {
+                var client = new RestClient(url);
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("content-type", "application/json");
+                request.AddParameter("application/json", json, ParameterType.RequestBody);
+
+                if (autorizacion != null)
+                {
+                    request.AddHeader("Authorization", autorizacion);
+                }
+
+                IRestResponse response = client.Execute(request);
+
+                dynamic datos = JsonConvert.DeserializeObject(response.Content);
+
+                return datos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+    }
+}
--- TurismoReal/Departamentos.cs
+++ TurismoReal/Departamentos.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,11 +14,14 @@ namespace TurismoReal
 {
     public partial class Departamentos : Form
     {
+        private ApiPost _apiPost;
         public Departamentos()
         {
             InitializeComponent();
+            _apiPost = new ApiPost();
         }
 
+        #region EVENTOS
         private async void btnMostrar_Click(object sender, EventArgs e)
         {
             var response = await RestHelper.MostrarDepto();
@@ -28,5 +33,80 @@ namespace TurismoReal
             var response = await RestHelper.BuscarDepto(txtBuscar.Text);
             rtbMostrar.Text = RestHelper.LectorJson(response);
         }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            AgregarDepto();
+            LimpiarCampos();
+        }
+
+        private async void btnEliminar_Click(object sender, EventArgs e)
+        {
+            var response = await EliminarDepto(txtBuscar.Text);
+            rtbMostrar.Text = response;
+        }
+        #endregion
+
+        #region METODOS
+        public class Departamento
+        {
+            public string direccion { get; set; }
+            public string zona { get; set; }
+            public int banos { get; set; }
+            public int dormitorios { get; set; }
+            public bool estado_mantencion { get; set; }
+            public int precio { get; set; }
+            public int metros_cuadrados { get; set; }
+        }
+        private void AgregarDepto()
+        {
+            Departamento depto = new Departamento
+            {
+                direccion = txtDireccion.Text,
+                zona = cbZona.Text,
+                banos = int.Parse(txtBanos.Text),
+                dormitorios = int.Parse(txtDorm.Text),
+                estado_mantencion = true,
+                precio = int.Parse(txtPrecio.Text),
+                metros_cuadrados = int.Parse(txtMetros.Text)
+            };
+
+            string json = JsonConvert.SerializeObject(depto);
+
+            dynamic respuesta = _apiPost.Agregar("https://departamentos.pythonanywhere.com/api/departamentos/", json);
+
+            MessageBox.Show("Departamento agregado correctamente", "Felicidades");
+        }
+
+        private async Task<string> EliminarDepto(string id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage res = await client.DeleteAsync("https://departamentos.pythonanywhere.com/api/departamentos/" + id))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        MessageBox.Show("Departamento eliminado correctamente", "Felicidades");
+                        string data = await content.ReadAsStringAsync();
+                        if (data != null)
+                        {
+                            return data;
+                        }
+                    }
+                }
+            }
+            return string.Empty;
+        }
+
+        public void LimpiarCampos ()
+        {
+            this.txtBanos.Text = "";
+            this.txtDireccion.Text = "";
+            this.txtDorm.Text = "";
+            this.txtMetros.Text = "";
+            this.txtPrecio.Text = "";
+            this.cbZona.Text = " Seleccionar zona";
+        }
+        #endregion
     }
 }
