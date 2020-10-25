03ef4f6a805f83d933c19cc8575f11f7d5d60f9f
anais <ana.acevedo@alumnos.duoc.cl>
2020-10-24 23:05:20 -0300

    termino crud depto

--- TurismoReal/Vistas/Funcionarios.cs
+++ TurismoReal/Vistas/Funcionarios.cs
@@ -24,7 +24,7 @@ namespace TurismoReal
         public Funcionarios()
         {
             InitializeComponent();
-            MostrarFunc();
+            MostrarFunc(false);
             rbActivo.Enabled = false;
             rbInactivo.Enabled = false;
             _apiPost = new ApiPost();
@@ -60,7 +60,7 @@ namespace TurismoReal
         #region EVENTOS
         private void btnMostrar_Click(object sender, EventArgs e)
         {
-            MostrarFunc();
+            MostrarFunc(false);
         }
 
         private void btnBuscar_Click(object sender, EventArgs e)
@@ -85,26 +85,28 @@ namespace TurismoReal
             {
                 AgregarFunc();
                 LimpiarCampos();
-                MostrarFunc();
+                MostrarFunc(false);
             }
         }
 
         private void btnEditar_Click(object sender, EventArgs e)
         {
-            //if (txtBuscar.Text == "")
-            //{
-            //    MessageBox.Show("Ingrese un Id para editar", "¡Error!");
-            //}
-            //else
-            //{
+            if (txtBuscar.Text == "")
+            {
+                MessageBox.Show("Ingrese un Id para editar", "¡Error!");
+            }
+            else
+            {
                 ModificarFunc();
-            MostrarFunc();
-            //}
+                LimpiarCampos();
+            }
         }
         #endregion
 
         #region METODOS 
-        private async void MostrarFunc()
+
+    
+        private async void MostrarFunc(bool filtro)
         {
             var response = await RestHelper.MostrarFunc();
             var result = JsonConvert.DeserializeObject<List<Funcionarioget>>(response);
@@ -128,35 +130,32 @@ namespace TurismoReal
             dgFunc.Columns[8].Width = 100;
             dgFunc.Columns[9].HeaderText = "Contraseña";
             dgFunc.Columns[9].Width = 80;
+            foreach (DataGridViewRow fila in dgFunc.Rows)
+            {
+                fila.Cells["password"].Value = "********";
+            }
+            dgFunc.Columns["is_active"].Visible = false;
+            DataGridViewTextBoxColumn textColumn = new DataGridViewTextBoxColumn();
+            textColumn.Name = "Estado";
+            dgFunc.Columns.Add(textColumn);
+            dgFunc.Columns["Estado"].DisplayIndex = 3;
+            foreach (DataGridViewRow fila in dgFunc.Rows.Cast<DataGridViewRow>())
+            {
+                fila.Cells["Estado"].Value = (bool)fila.Cells["is_active"].Value ? "Activo" : "Inactivo";
+            }
+            if (filtro == true)
+            {
+                dgFunc.CurrentCell = null;
+                foreach (DataGridViewRow fila in dgFunc.Rows)
+                {
+                    fila.Visible = fila.Cells["id"].Value.ToString().ToUpper().Equals(txtBuscar.Text.ToUpper());
+                }
+            }
         }
 
         private async void BuscarFunc()
         {
             var response = await RestHelper.BuscarFunc(txtBuscar.Text);
-
-            var result = JsonConvert.DeserializeObject<List<Funcionarioget>>(response);
-            dgFunc.DataSource = result;
-            dgFunc.Columns[0].HeaderText = "id";
-            dgFunc.Columns[0].Width = 30;
-            dgFunc.Columns[1].HeaderText = "Nombre";
-            dgFunc.Columns[1].Width = 100;
-            dgFunc.Columns[2].HeaderText = "Apellido";
-            dgFunc.Columns[2].Width = 100;
-            dgFunc.Columns[3].HeaderText = "Estado";
-            dgFunc.Columns[3].Width = 50;
-            dgFunc.Columns[4].Visible = false;
-            dgFunc.Columns[5].HeaderText = "Teléfono";
-            dgFunc.Columns[5].Width = 80;
-            dgFunc.Columns[6].HeaderText = "Edad";
-            dgFunc.Columns[6].Width = 50;
-            dgFunc.Columns[7].HeaderText = "Correo personal";
-            dgFunc.Columns[7].Width = 100;
-            dgFunc.Columns[8].HeaderText = "Correo empresarial";
-            dgFunc.Columns[8].Width = 100;
-            dgFunc.Columns[9].HeaderText = "Contraseña";
-            dgFunc.Columns[9].Width = 80;
-
-            //rtbMostrar.Text = RestHelper.LectorJson(response);
             var datos = JsonConvert.DeserializeObject<Funcionario>(response);
             this.txtNombre.Text = datos.first_name;
             this.txtApellido.Text = datos.last_name;
@@ -164,9 +163,10 @@ namespace TurismoReal
             this.txtFono.Text = datos.telefono.ToString();
             this.txtCorreo.Text = datos.email;
             this.txtEmp.Text = datos.username;
-            this.txtCont.Text = datos.password;
+            this.txtCont.Text ="";
             this.rbActivo.Enabled = true;
             this.rbInactivo.Enabled = true;
+            MostrarFunc(true);
         }
 
         private void AgregarFunc()
@@ -188,12 +188,13 @@ namespace TurismoReal
             MessageBox.Show($"Usuario {func.first_name + " " + func.last_name} agregado correctamente", "¡Éxito!");
         }
 
-        private void ModificarFunc()
+        private async void ModificarFunc()
         {
             Funcionario func = new Funcionario
             {
                 first_name = txtNombre.Text,
                 last_name = txtApellido.Text,
+                is_active = true,
                 is_staff = true,
                 telefono = int.Parse(txtFono.Text),
                 edad = int.Parse(txtEdad.Text),
@@ -201,11 +202,26 @@ namespace TurismoReal
                 email = txtCorreo.Text,
                 password = txtCont.Text,
             };
-            string json = JsonConvert.SerializeObject(func);
-            dynamic respuesta = _apiPut.Modificar($"https://departamentos.pythonanywhere.com/api/usuarios/{txtBuscar.Text}/", json);
-            MessageBox.Show($"Usuario {func.first_name + " " + func.last_name} modificado correctamente", "¡Éxito!");
+            if(rbInactivo.Checked)
+            {
+                func.is_active = false;
+            }
+
+            var response = await RestHelper.BuscarFunc(txtBuscar.Text);
+            var datos = JsonConvert.DeserializeObject<Funcionario>(response);
+            if (func.password == "")
+            {
+                MessageBox.Show($"Para modificar el usuario se necesita cambiar la clave", "¡Error!");
+            }
+            else
+            {
+                string json = JsonConvert.SerializeObject(func);
+                dynamic respuesta = _apiPut.Modificar($"https://departamentos.pythonanywhere.com/api/usuarios/{txtBuscar.Text}/", json);
+                MessageBox.Show($"Usuario {func.first_name + " " + func.last_name} modificado correctamente", "¡Éxito!");
+                MostrarFunc(true);
+            }
         }
-        
+
         public void LimpiarCampos()
         {
             this.txtApellido.Text = "";
